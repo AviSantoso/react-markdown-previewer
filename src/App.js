546f3c79126695b0c099b20e6b833cefc4e2bef7
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-07 06:29:15 +0800

    finish functionality

--- src/App.js
+++ src/App.js
@@ -1,52 +1,84 @@
-import { makeStyles, Grid, Paper, Box } from "@material-ui/core";
+import React from "react";
+import { makeStyles, Grid, Paper, Box, TextField } from "@material-ui/core";
+import marked from "marked";
+
+import defaultMarkdown from "./sample.md";
 
 import "./styles.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-    minWidth: "80vw",
-    minHeight: "80vh",
-    margin: "auto"
+    flexGrow: 1
   },
   paper: {
-    minHeight: "94vh",
-    width: "47vw"
+    width: "100%"
+  },
+  box: {
+    padding: "1em",
+    margin: "1em",
+    width: "100%"
   },
   editor: {
-    width: "99%",
-    resize: "none",
-    border: "1px solid #FAFAFA",
-    borderRadius: "20px",
-    fontSize: "18px",
-    lineHeight: "1.5em"
+    width: "100%"
   }
 }));
 
+marked.setOptions({
+  gfm: true,
+  breaks: true
+});
+
 export default function App() {
+  const [markdown, setMarkdown] = React.useState(defaultMarkdown);
+  const editorRef = React.useRef();
+  const textFieldRef = React.useRef();
   const classes = useStyles();
+
+  const onEditorChanged = () => {
+    setMarkdown(editorRef.current.value);
+  };
+  const onTextFieldChanged = () => {
+    setMarkdown(textFieldRef.current.value);
+  };
+
   return (
     <div className="app">
-      <Grid container className={classes.root} spacing={2}>
+      <Grid container className={classes.root} spacing={3} margin={3}>
         <Grid item xs={12}>
-          <Grid container justify="center" alignItems="center" spacing={2}>
-            <Grid item>
-              <Paper className={classes.paper}>
-                <Box spacing={3}>
-                  <textarea
-                    id="editor"
-                    className={classes.editor}
-                    rows="28"
-                  ></textarea>
-                </Box>
-              </Paper>
-            </Grid>
-            <Grid item>
-              <Paper className={classes.paper}>
-                <div id="preview"></div>
-              </Paper>
-            </Grid>
-          </Grid>
+          <Paper className={classes.paper}>
+            <Box className={classes.box}>
+              <textarea
+                id="editor"
+                ref={editorRef}
+                onChange={onEditorChanged}
+                value={markdown}
+                hidden
+              ></textarea>
+              <TextField
+                id="editor-textfield"
+                className={classes.editor}
+                label="Markdown"
+                variant="outlined"
+                multiline
+                rows={20}
+                inputRef={textFieldRef}
+                onChange={onTextFieldChanged}
+                value={markdown}
+              />
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Box className={classes.box}>
+              <div
+                id="preview"
+                dangerouslySetInnerHTML={{
+                  __html: marked(markdown ?? "")
+                }}
+              />
+            </Box>
+          </Paper>
         </Grid>
       </Grid>
     </div>
